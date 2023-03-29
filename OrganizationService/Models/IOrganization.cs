686f3497755d2f881407f6a6b1edd9d7214712ff
nkrumah <79527821+vsvincent@users.noreply.github.com>
2023-03-29 18:16:37 +0200

    Create Organization microservice proof of concept

---
+++ OrganizationService/Models/IOrganization.cs
@@ -0,0 +1,11 @@
+﻿namespace OrganizationService.Models
+{
+    public interface IOrganization
+    {
+        public string? Code { get; }
+        public string? Name { get; set; }
+        public ICollection<string>? AdministratorIds { get; set; }
+        public ICollection<string>? ManagerIds { get; set; }
+        public ICollection<string>? ObserverIds { get; set; }
+    }
+}
