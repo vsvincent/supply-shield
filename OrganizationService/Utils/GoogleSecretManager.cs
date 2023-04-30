8f7f0583b231d27be1399082a579587d1bb8d8e9
v.vonschilling@student.fontys.nl <v.vonschilling@student.fontys.nl>
2023-04-30 15:30:12 +0300

    Update GoogleSecretManager.cs

--- OrganizationService/Utils/GoogleSecretManager.cs
+++ OrganizationService/Utils/GoogleSecretManager.cs
@@ -10,21 +10,16 @@ namespace OrganizationService.Utils
         public string AccessId { get; private set; } = "QLDBAccess";
         public Secret GetSecretMetadata(string secretId)
         {
-            // Create the client.
             SecretManagerServiceClient client = SecretManagerServiceClient.Create();
 
-            // Build the resource name.
             SecretName secretName = new SecretName(projectId, secretId);
 
-            // Call the API.
             Secret secret = client.GetSecret(secretName);
             return secret;
         }
         public string GetSecret(string secretId, string secretVersionId)
         {
-            // Create the client.
             SecretManagerServiceClient client = SecretManagerServiceClient.Create();
-            // Call the API.
             AccessSecretVersionResponse result =  client.AccessSecretVersion(new SecretVersionName(projectId, secretId, secretVersionId));
             return result.Payload.Data.ToStringUtf8();
         }
