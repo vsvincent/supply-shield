bd6864436983e6daf3f3ce2d3696c82ecbcf3d86
v.vonschilling@student.fontys.nl <v.vonschilling@student.fontys.nl>
2023-06-11 21:43:28 +0200

    Add incident controller, refactor firebase authentication

--- Gateway/Controllers/IncidentController.cs
+++ Gateway/Controllers/IncidentController.cs
@@ -1,6 +1,33 @@
-﻿namespace Gateway.Controllers
+﻿using Gateway.Extensions;
+using Gateway.Models;
+using Gateway.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gateway.Controllers
 {
-    public class IncidentController
+    [ApiController]
+    [Route("[controller]")]
+    public class IncidentController : ControllerBase
     {
+        private readonly ILogger<IncidentController> _logger;
+        private readonly CommunicationService communicationService = new CommunicationService();
+        public IncidentController(ILogger<IncidentController> logger)
+        {
+            _logger = logger;
+        }
+        [Authorize]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            FirebaseUser user = HttpContext.GetFirebaseUser();
+
+            return Ok(user);
+        }
+        [HttpGet("test")]
+        public async Task<IActionResult> GetTest()
+        {
+            return Ok(await communicationService.GetItem<String>("wow"));
+        }
     }
 }
--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,5 +1,6 @@
 using FirebaseAdmin;
 using Gateway.Extensions;
+using Google.Apis.Auth.OAuth2;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
@@ -9,7 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddSingleton(FirebaseApp.Create());
+builder.Services.AddSingleton(FirebaseApp.Create(new AppOptions
+{
+    Credential = GoogleCredential.GetApplicationDefault(),
+    ProjectId = "supply-shield-381721"
+}));
 builder.Services.AddFirebaseAuthentication();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
